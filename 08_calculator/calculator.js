e8b46cb4a5b60922e0d012f3c50bdb007d5bf643
Bansker <jud.yannick@protonmail.ch>
2024-07-08 16:07:11 +0200

    solved excercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,60 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+
+const sum = function(nums) {
+  let retVal;
+  if(nums.length === 0) {
+    retVal = 0;
+
+  } else if (nums.length === 1) {
+    retVal = nums[0];
+
+  } else {
+    retVal = nums.reduce((accumulator, nextNum) => {
+      return accumulator + nextNum;
+    });
+  }
+
+  return retVal;
 };
 
-const multiply = function() {
 
+const multiply = function(nums) {
+  return nums.reduce((accumulator, nextNum) => accumulator * nextNum);
 };
 
-const power = function() {
-	
+
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+
+const factorial = function(x) {
+  let retVal;
+	if(x < 2) {
+    retVal = 1; // 0! and 1! = 1
+
+  } else {
+    let factorized = 1;
+
+    for(let i = 1; i <= x; i++) {
+      factorized *= i;
+    }
+    retVal = factorized;
+  }
+
+  return retVal;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
