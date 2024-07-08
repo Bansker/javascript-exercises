844d7207b18946c4691a2b4445c35fb4850fbf3a
Bansker <jud.yannick@protonmail.ch>
2024-07-08 22:09:02 +0200

    solved excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(memberNum) {
+  if(memberNum < 0) return "OOPS";
 
+  let fibonacciSeq = [0];
+  let fibNext = 1;
+  for(let i = 1; i <= memberNum; i++) {
+    fibonacciSeq.push(fibonacciSeq[i - 1] + fibNext);
+    fibNext = fibonacciSeq[i - 1];
+  }
+  return fibonacciSeq[fibonacciSeq.length - 1];
 };
 
 // Do not edit below this line
