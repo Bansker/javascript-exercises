2f1104b320d2245c4820b38ad771004d51f1cd7e
Bansker <jud.yannick@protonmail.ch>
2024-07-08 16:35:10 +0200

    solved excercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (msg) {
+  msg = msg.toLowerCase().replace(/[\s.,!]/g, '');
+  let flippedMsg = msg.split('').reverse().join('');
 
+  console.log(msg);
+  console.log(flippedMsg)
+  if(flippedMsg == msg) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
