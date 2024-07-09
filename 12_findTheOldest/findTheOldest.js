2e2202262693d328911d6101f96e65bce9ad8722
Bansker <jud.yannick@protonmail.ch>
2024-07-09 13:33:26 +0200

    working on excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+
+
+const findTheOldest = function(people) {
+
+
+
 
 };
+
+
+
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+  },
+]
+
+console.log(findTheOldest(people));
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
